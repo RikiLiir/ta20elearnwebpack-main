80021c49abc8628fbf01ed9c03e9455577330970
RikiLiir <73335232+RikiLiir@users.noreply.github.com>
2023-01-31 13:18:29 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+let htmlPlugins = glob.sync('./src/views/*',{nodir: true}).map(templatePath => {
+    return new HtmlWebpackPlugin({
+        template: templatePath,
+        filename: path.parse(templatePath).name + '.html',
+        templateParameters: {
+            hello: 'somecoolshite'
+        },
+    });
+});
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader","css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader","css-loader","sass-loader"],
+            },
+            {
+                test: /\.(njk|nunjucks)$/i,
+                use: ["simple-nunjucks-loader"],
+            },
+        ],
+    },
+    plugins: [
+       ...htmlPlugins,
+    ],
+};
